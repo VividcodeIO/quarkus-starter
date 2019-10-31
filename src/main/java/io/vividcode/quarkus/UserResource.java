26767cf4c8a8937f4ab2c541576cf1ca5eac1b83
Fu Cheng <alexcheng1982@gmail.com>
2019-10-31 13:32:07 +1300

    Remove unused methods

--- src/main/java/io/vividcode/quarkus/UserResource.java
+++ src/main/java/io/vividcode/quarkus/UserResource.java
@@ -10,8 +10,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 @Path("/user")
 @Produces(MediaType.APPLICATION_JSON)
@@ -35,15 +33,5 @@ public class UserResource {
   @DELETE
   public void delete(@PathParam("id") String id) {
     userService.delete(id);
-  }
-
-  @Path("{id}")
-  public Response getUser(@PathParam("id") String userId) {
-    User user = userService.findById(userId);
-    if (user != null) {
-      return Response.ok(user).build();
-    } else {
-      return Response.status(Status.NOT_FOUND).build();
-    }
   }
 }
--- src/main/java/io/vividcode/quarkus/UserService.java
+++ src/main/java/io/vividcode/quarkus/UserService.java
@@ -25,14 +25,6 @@ public class UserService {
     return user;
   }
 
-  public User findById(String id) {
-    return users.get(id);
-  }
-
-  public User deleteUser(String userId) {
-    return users.remove(userId);
-  }
-
   public List<User> list() {
     CriteriaQuery<User> query = entityManager.getCriteriaBuilder().createQuery(User.class);
     TypedQuery<User> allQuery = entityManager.createQuery(query.select(query.from(User.class)));
